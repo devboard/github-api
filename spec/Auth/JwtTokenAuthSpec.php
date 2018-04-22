6c08191a8c29d4801adb37488242385663e2f409
Miro Svrtan <miro@mirosvrtan.me>
2018-04-22 17:12:25 +0200

    Feat #3: Add Auth

---
+++ spec/Auth/JwtTokenAuthSpec.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\DevboardLib\GitHubApi\Auth;
+
+use DevboardLib\GitHubApi\Auth\AuthMethod;
+use DevboardLib\GitHubApi\Auth\JwtTokenAuth;
+use PhpSpec\ObjectBehavior;
+
+class JwtTokenAuthSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beConstructedWith($token = 'a1b2c3d4-fake-jwt-github-token');
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(JwtTokenAuth::class);
+        $this->shouldImplement(AuthMethod::class);
+    }
+
+    public function it_returns_token_when_asked_for_token_or_login()
+    {
+        $this->getTokenOrLogin()->shouldReturn('a1b2c3d4-fake-jwt-github-token');
+    }
+
+    public function it_returns_token()
+    {
+        $this->getToken()->shouldReturn('a1b2c3d4-fake-jwt-github-token');
+    }
+
+    public function it_returns_null_as_jwt_has_no_login()
+    {
+        $this->getLogin()->shouldReturn(null);
+    }
+
+    public function it_returns_null_as_jwt_has_no_password()
+    {
+        $this->getPassword()->shouldReturn(null);
+    }
+
+    public function it_returns_auth_method()
+    {
+        $this->getAuthMethod()->shouldReturn('jwt');
+    }
+}
