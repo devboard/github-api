8ddfdba0e70b8edbec360b78eea98a0dbf81bad8
Miro Svrtan <miro@mirosvrtan.me>
2018-05-01 19:59:50 +0200

    Feat #10: PullRequest support

--- tests/V4/TestData/TestDataProvider.php
+++ tests/V4/TestData/TestDataProvider.php
@@ -55,4 +55,20 @@ class TestDataProvider
 
         return $results;
     }
+
+    public function getGitHubV4PullRequestData(): array
+    {
+        $results = [];
+
+        $finder = new Finder();
+
+        $files = $finder->files()->in(__DIR__)->name('pullrequests.json')->getIterator();
+
+        foreach ($files as $file) {
+            $data      = json_decode($file->getContents(), true);
+            $results[] = $data;
+        }
+
+        return $results;
+    }
 }
