2d364a74dcdd30502c88e31f189009a68d28a738
Miro Svrtan <miro@mirosvrtan.me>
2018-05-05 23:39:24 +0200

    Add Query

---
+++ src/V4/Query/Repository/RepositoryQuery.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DevboardLib\GitHubApi\V4\Query\Repository;
+
+use DevboardLib\GitHubApi\V4\Query\Query;
+
+interface RepositoryQuery extends Query
+{
+}
