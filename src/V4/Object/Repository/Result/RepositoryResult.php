8a8ef52110bdf47a46a10481a0dc5a007c249f04
Miro Svrtan <miro@mirosvrtan.me>
2018-06-03 13:57:27 +0200

    RepositoryResult should extend QueryResult

--- src/V4/Object/Repository/Result/RepositoryResult.php
+++ src/V4/Object/Repository/Result/RepositoryResult.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace DevboardLib\GitHubApi\V4\Object\Repository\Result;
 
+use DevboardLib\Generix\QueryResult;
 use DevboardLib\GitHub\Repo\RepoFullName;
-use DevboardLib\GitHubApi\Result\Result;
 
-interface RepositoryResult extends Result
+interface RepositoryResult extends QueryResult
 {
     public function getRepoFullName(): RepoFullName;
 }
