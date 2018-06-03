b6283ab0aed8c24afb53c47d21af2ccf24dbd151
Miro Svrtan <miro@mirosvrtan.me>
2018-06-03 13:24:59 +0200

    Define RepositoryQuery expectations
    RepositoryQuery should expose owner name, repo name and full name +
    credentials to fetch them from remote system

--- src/Query/Repository/RepositoryQuery.php
+++ src/Query/Repository/RepositoryQuery.php
@@ -5,7 +5,18 @@ declare(strict_types=1);
 namespace DevboardLib\GitHubApi\Query\Repository;
 
 use DevboardLib\Generix\QueryRequest;
+use DevboardLib\GitHub\Account\AccountLogin;
+use DevboardLib\GitHub\Repo\RepoFullName;
+use DevboardLib\GitHub\Repo\RepoName;
+use DevboardLib\GitHubApi\Credentials\InstallationCredentials;
 
 interface RepositoryQuery extends QueryRequest
 {
+    public function getOwnerName(): AccountLogin;
+
+    public function getRepoName(): RepoName;
+
+    public function getCredentials(): InstallationCredentials;
+
+    public function getRepoFullName(): RepoFullName;
 }
