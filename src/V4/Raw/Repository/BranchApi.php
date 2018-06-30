f982a8176f6a418d097866a792807f4069970fb0
Miro Svrtan <miro@mirosvrtan.me>
2018-06-30 23:28:07 +0200

    Adding allBranches() and deprecating old one

--- src/V4/Raw/Repository/BranchApi.php
+++ src/V4/Raw/Repository/BranchApi.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace DevboardLib\GitHubApi\V4\Raw\Repository;
 
+use DevboardLib\GitHub\Repo\RepoFullName;
+use DevboardLib\GitHubApi\Credentials\InstallationCredentials;
 use DevboardLib\GitHubApi\Query\Repository\Request\AllBranchesQuery;
 use DevboardLib\GitHubApi\V3\GitHubClientFactory;
 
@@ -17,6 +19,24 @@ class BranchApi
         $this->clientFactory = $clientFactory;
     }
 
+    public function allBranches(RepoFullName $fullName, InstallationCredentials $credentials): array
+    {
+        $queryDefinition = file_get_contents(__DIR__.'/branches.graphql');
+
+        $variables = [
+            'owner' => $fullName->getOwner()->__toString(),
+            'name'  => $fullName->getRepoName()->__toString(),
+        ];
+        $client = $this->clientFactory->createAppAndUserAuthenticatedClient($credentials);
+
+        $data = $client->graphql()->execute($queryDefinition, $variables);
+
+        return $data;
+    }
+
+    /**
+     * @deprecated REMOVE THIS ONE IN VERSION 2.0
+     */
     public function handleAllBranchesQuery(AllBranchesQuery $query): array
     {
         $queryDefinition = file_get_contents(__DIR__.'/branches.graphql');
