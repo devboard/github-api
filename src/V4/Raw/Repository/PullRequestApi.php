150c468bf90e814c79cb5495c501c25a6ca41c41
Miro Svrtan <miro@mirosvrtan.me>
2018-06-30 23:33:41 +0200

    Adding allPullRequests() and deprecating old one

--- src/V4/Raw/Repository/PullRequestApi.php
+++ src/V4/Raw/Repository/PullRequestApi.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace DevboardLib\GitHubApi\V4\Raw\Repository;
 
+use DevboardLib\GitHub\Repo\RepoFullName;
+use DevboardLib\GitHubApi\Credentials\InstallationCredentials;
 use DevboardLib\GitHubApi\Query\Repository\Request\AllPullRequestsQuery;
 use DevboardLib\GitHubApi\V3\GitHubClientFactory;
 use Github\Api\GraphQL;
@@ -18,6 +20,25 @@ class PullRequestApi
         $this->clientFactory = $clientFactory;
     }
 
+    public function allPullRequests(RepoFullName $fullName, InstallationCredentials $credentials, $cursor = null): array
+    {
+        $queryDefinition = file_get_contents(__DIR__.'/pull_requests.graphql');
+
+        $variables = [
+            'owner'  => $fullName->getOwner()->__toString(),
+            'name'   => $fullName->getRepoName()->__toString(),
+            'cursor' => $cursor,
+        ];
+        $client = $this->clientFactory->createAppAndUserAuthenticatedClient($credentials);
+
+        $data = $client->graphql()->execute($queryDefinition, $variables);
+
+        return $data;
+    }
+
+    /**
+     * @deprecated REMOVE THIS ONE IN VERSION 2.0
+     */
     public function handleAllPullRequestsQuery(AllPullRequestsQuery $query): array
     {
         $queryDefinition = file_get_contents(__DIR__.'/pull_requests.graphql');
