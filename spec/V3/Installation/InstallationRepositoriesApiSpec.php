aef9bf7f33cc6721e42e2de19baebf7a91f95e73
Miro Svrtan <miro@mirosvrtan.me>
2018-07-20 16:56:57 +0200

    Cleaning V3 API from deprecated methods

--- spec/V3/Installation/InstallationRepositoriesApiSpec.php
+++ spec/V3/Installation/InstallationRepositoriesApiSpec.php
@@ -6,7 +6,6 @@ namespace spec\DevboardLib\GitHubApi\V3\Installation;
 
 use DevboardLib\GitHubApi\Credentials\InstallationCredentials;
 use DevboardLib\GitHubApi\V3\GitHubClientFactory;
-use DevboardLib\GitHubApi\V3\Installation\Factory\GitHubRepoFactory;
 use DevboardLib\GitHubApi\V3\Installation\InstallationRepositoriesApi;
 use Github\Api\Apps as AppsApi;
 use Github\Client;
@@ -14,9 +13,9 @@ use PhpSpec\ObjectBehavior;
 
 class InstallationRepositoriesApiSpec extends ObjectBehavior
 {
-    public function let(GitHubClientFactory $clientFactory, GitHubRepoFactory $gitHubRepoFactory)
+    public function let(GitHubClientFactory $clientFactory)
     {
-        $this->beConstructedWith($clientFactory, $gitHubRepoFactory);
+        $this->beConstructedWith($clientFactory);
     }
 
     public function it_is_initializable()
@@ -40,23 +39,5 @@ class InstallationRepositoriesApiSpec extends ObjectBehavior
             ->willReturn(['repositories' => []]);
 
         $this->allInstallationRepositories($credentials)->shouldReturn([]);
-    }
-
-    public function it_retrieves_installation_repositories_accessible_by_user(
-        InstallationCredentials $credentials, GitHubClientFactory $clientFactory, Client $client, AppsApi $appsApi
-    ) {
-        $clientFactory->createAppAndUserAuthenticatedClient($credentials)
-            ->shouldBeCalled()
-            ->willReturn($client);
-
-        $client->apps()
-            ->shouldBeCalled()
-            ->willReturn($appsApi);
-
-        $appsApi->listRepositories()
-            ->shouldBeCalled()
-            ->willReturn(['repositories' => []]);
-
-        $this->fetch($credentials);
     }
 }
