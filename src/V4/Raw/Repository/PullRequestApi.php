01ec1280d9261e539faa670066fa90a014654f92
Miro Svrtan <miro@mirosvrtan.me>
2018-07-20 16:13:53 +0200

    Removing deprecated API endpoints

--- src/V4/Raw/Repository/PullRequestApi.php
+++ src/V4/Raw/Repository/PullRequestApi.php
@@ -6,7 +6,6 @@ namespace DevboardLib\GitHubApi\V4\Raw\Repository;
 
 use DevboardLib\GitHub\Repo\RepoFullName;
 use DevboardLib\GitHubApi\Credentials\InstallationCredentials;
-use DevboardLib\GitHubApi\Query\Repository\Request\AllPullRequestsQuery;
 use DevboardLib\GitHubApi\V3\GitHubClientFactory;
 use Github\Api\GraphQL;
 
@@ -34,38 +33,5 @@ class PullRequestApi
         $data = $client->graphql()->execute($queryDefinition, $variables);
 
         return $data;
-    }
-
-    /**
-     * @deprecated REMOVE THIS ONE IN VERSION 2.0
-     */
-    public function handleAllPullRequestsQuery(AllPullRequestsQuery $query): array
-    {
-        $queryDefinition = file_get_contents(__DIR__.'/pull_requests.graphql');
-
-        $variables = [
-            'owner'  => (string) $query->getOwnerName(),
-            'name'   => (string) $query->getRepoName(),
-            'cursor' => null,
-        ];
-
-        $client = $this->clientFactory->createAppAndUserAuthenticatedClient($query->getCredentials());
-
-        $results = [];
-
-        do {
-            $data = $client->graphql()->execute($queryDefinition, $variables);
-
-            $results[] = $data;
-
-            if (false === $data['data']['repository']['pullRequests']['pageInfo']['hasNextPage']) {
-                break;
-            }
-            $lastId = $data['data']['repository']['pullRequests']['pageInfo']['endCursor'];
-
-            $variables['cursor'] = $lastId;
-        } while (true);
-
-        return $results;
     }
 }
